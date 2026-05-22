fbf144a08bf71b4a2483df6ee1f6ee7c54d78bb7
IkerRL <163485280+IkerRL@users.noreply.github.com>
2026-05-22 18:19:01 +0200

    Update judges' names and images in juecesConfig

--- script.js
+++ script.js
@@ -13,9 +13,9 @@ const IMAGEN_PRE_VOTO = 'revelar_icono.png';
 
 const juecesConfig = [
     { id: 1, nombre: 'Valeria', img: 'juez1.png' },
-    { id: 2, nombre: 'Iker',    img: 'juez2.png' },
-    { id: 3, nombre: 'Juez 3',  img: 'juez3.png' },
-    { id: 4, nombre: 'Juez Pito', img: 'juez4.png' },
+    { id: 2, nombre: 'Cris',    img: 'juez2.png' },
+    { id: 3, nombre: 'Iker',  img: 'juez3.jpg' },
+    { id: 4, nombre: 'Luve', img: 'juez4.png' },
 ];
 
 const cancionesData = [
