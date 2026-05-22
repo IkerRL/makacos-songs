4a5a119d44cc75db2ee3450e3b9812a4637b6b8b
IkerRL <163485280+IkerRL@users.noreply.github.com>
2026-05-22 18:13:15 +0200

    Update judge images from .jpg to .png

--- script.js
+++ script.js
@@ -12,10 +12,10 @@ const TWITCH_CONFIG = {
 const IMAGEN_PRE_VOTO = 'revelar_icono.png'; 
 
 const juecesConfig = [
-    { id: 1, nombre: 'Valeria', img: 'juez1.jpg' },
-    { id: 2, nombre: 'Iker',    img: 'juez2.jpg' },
-    { id: 3, nombre: 'Juez 3',  img: 'juez3.jpg' },
-    { id: 4, nombre: 'Juez Pito', img: 'juez4.jpg' },
+    { id: 1, nombre: 'Valeria', img: 'juez1.png' },
+    { id: 2, nombre: 'Iker',    img: 'juez2.png' },
+    { id: 3, nombre: 'Juez 3',  img: 'juez3.png' },
+    { id: 4, nombre: 'Juez Pito', img: 'juez4.png' },
 ];
 
 const cancionesData = [
